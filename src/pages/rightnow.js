5317f2e54ef6624cdf1eb64966c944164627166c
Albany Patriawan <zekromfx1337@gmail.com>
2024-02-15 00:20:12 -0800

    combined components together

--- src/pages/rightnow.js
+++ src/pages/rightnow.js
@@ -1,9 +1,56 @@
-import Navbar from "@/components/Navbar"
+import Navbar from "@/components/Navbar";
+import ClassHeader from "./RightNow/ClassHeader/ClassHeader";
+import StudentList from "@/components/StudentList";
+import styles from "@/styles/RightNow.module.css";
+
+let students = [
+  {
+    path: "/favicon.ico",
+    name: "Alice Johnson",
+    info: "Arrived 11:50pm",
+  },
+  {
+    path: "/favicon.ico",
+    name: "Bob Smith",
+    info: "Arrived 11:50pm",
+  },
+  {
+    path: "/favicon.ico",
+    name: "Eva Lee",
+    info: "Arrived 11:50pm",
+  },
+  {
+    path: "/favicon.ico",
+    name: "Alice Johnson",
+    info: "Arrived 11:50pm",
+  },
+  {
+    path: "/favicon.ico",
+    name: "Bob Smith",
+    info: "Arrived 11:50pm",
+  },
+  {
+    path: "/favicon.ico",
+    name: "Eva Lee",
+    info: "Arrived 11:50pm",
+  },
+];
 
 export default function Rightnow() {
-    return(
-        <>
-            <Navbar />
-        </>
-    )
+  return (
+    <>
+      <div className={styles["right-now-container"]}>
+        <Navbar />
+        <div className={styles["right-now-container__content"]}>
+          <ClassHeader className={styles["class-header"]} />
+          <div
+            className={styles["right-now-container__content__student-lists"]}
+          >
+            <StudentList students={students} label="Present" />
+            <StudentList students={students} label="Absent" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
