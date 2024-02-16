95771f51f8870318d2efc36df614e71cc9c08313
Hammad Shami <H2Shami@gmail.com>
2024-02-16 15:24:09 -0800

    bootstrap icons now imported

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-// import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
