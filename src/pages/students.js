213b8ce4f8f5a37d31f7f13cc2f9647b772b1027
Shaiv Pandya <51866463+ShaivPandya@users.noreply.github.com>
2024-03-04 07:33:17 -0800

    Create students page

--- src/pages/students.js
+++ src/pages/students.js
@@ -1,9 +1,38 @@
 import Navbar from "@/components/Navbar"
+import StudentList from "@/components/StudentList"
+
+let students = [
+    {
+      path: "/favicon.ico",
+      name: "Alyssa Johnson",
+    },
+    {
+      path: "/favicon.ico",
+      name: "Bob Smith",
+    },
+    {
+      path: "/favicon.ico",
+      name: "Eva Lee",
+    },
+    {
+      path: "/favicon.ico",
+      name: "Hammy Shammy",
+    },
+    {
+      path: "/favicon.ico",
+      name: "Bob Smithy",
+    },
+    {
+      path: "/favicon.ico",
+      name: "Eva Leenie",
+    },
+  ];
 
 export default function Students() {
     return(
         <>
             <Navbar />
+            <StudentList students={students} label="Students" />
         </>
     )
 }
