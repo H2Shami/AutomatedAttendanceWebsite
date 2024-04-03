1a0af51c8bf03bb7cac6b4b21985b00991d46ceb
Albany Patriawan <zekromfx1337@gmail.com>
2024-04-03 10:03:46 -0700

    Front-end subscribers to the back-end "check db" endpoint

--- src/pages/rightnow.js
+++ src/pages/rightnow.js
@@ -48,16 +48,6 @@ let students = [
   },
 ];
 
-// const attendanceRecords = [{ studentid: 987987987 }, { studentid: 123123123 }];
-
-// async function getAttendance() {
-//   const link = new URL(
-//     `http://localhost:3000/api/getAttendanceForMeeting?classid=${currentClass.classid}&meetingDate=${currentClass.meetingDate}&meetingStart=${currentClass.meetingStart}&meetingEnd=${currentClass.meetingEnd}`
-//   );
-//   const attendance = await fetch(link);
-//   return attendance;
-// }
-
 export default function Rightnow({ studentTable }) {
   const [attendance, setAttendance] = useState([]);
   const [present, setPresent] = useState([]);
@@ -75,43 +65,27 @@ export default function Rightnow({ studentTable }) {
         .then((record) => {
           setAttendance(record);
         });
-      // result.then((result) => {
-      //   setAttendance(result);
-      // });
     };
 
     // call expression
     getAttendance();
 
-    // Set interval to call fetchData every 5 seconds
-    const intervalId = setInterval(getAttendance, 8000);
-
-    // Clear interval when component unmounts
-    return () => clearInterval(intervalId);
-
     /* -=-=-=-=- Below is for dynamic updates (don't remove)-=-=-=--=*/
-    // const eventSource = new EventSource(
-    //   "http://localhost:8000/attendance_record"
-    // );
+    const eventSource = new EventSource("http://localhost:8000/check_db");
 
-    // eventSource.onmessage = (event) => {
-    //   // Start a refresh
-    //   console.log("event received");
-    //   // Query: All attendance records for this class_id WHERE the timestamp is for the current class_time and date
+    eventSource.onmessage = (event) => {
+      // Start a refresh
+      console.log("event received");
+      // Query: All attendance records for this class_id WHERE the timestamp is for the current class_time and date
+      getAttendance();
+    };
 
-    //   setPresent(
-    //     studentTable.filter((student) => {
-    //       return studentInList(student.student_id, attendanceRecords);
-    //     })
-    //   );
-    //   setAbsent(
-    //     studentTable.filter((student) => {
-    //       return !studentInList(student.student_id, attendanceRecords);
-    //     })
-    //   );
-    // };
+    eventSource.onerror = () => {
+      console.log("Event source error");
+      eventSource.close();
+    };
 
-    // return () => eventSource.close();
+    return () => eventSource.close();
   }, []);
 
   /* 2. Update the page if we find new attendees (i.e: when the above use effect is called)*/
@@ -128,15 +102,6 @@ export default function Rightnow({ studentTable }) {
     );
   }, [attendance]);
 
-  /* -=-=-=-=-=- Refresh components -- Bottom-up  -=-=-=-=-=-=*/
-  /* UseEffect subscribes to changes in "AttendanceRecords" */
-  /* AttendanceRecords is the STATE of the students who are present for this current class meeting and is fetched from the DB with a query */
-  /* The DB fetch is handled by a server-state management tool like react-query, which can be used to initialize refetches*/
-  /* React-query initializes a refetch when we receive a signal from the server, tellling us to update the DB*/
-  /* The signal from the server is sent whenever Nano pings attendance insertion endpoint*/
-
-  // console.log(JSON.stringify(studentTable, null, 2));
-  // todo: split student table into students who are here and aren't
   return (
     <>
       <div className={styles["right-now-container"]}>
