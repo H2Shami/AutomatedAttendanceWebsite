137fd7e706b01a27c572880a327270ee73c0e932
Albany Patriawan <zekromfx1337@gmail.com>
2024-04-27 14:50:05 -0700

    load classes from DB

--- src/pages/courses.js
+++ src/pages/courses.js
@@ -2,63 +2,53 @@ import Navbar from "@/components/Navbar";
 import styles from "@/styles/Courses.module.css";
 import DividerSvg from "../icons/Divider.jsx";
 import { FaPlus } from "react-icons/fa";
+import { PrismaClient } from "@prisma/client";
 import { arrayOf } from "prop-types";
+import Link from "next/link";
 
-export default function Courses() {
+// var data = [
+//   {
+//     classid: 2222,
+//     color: "#3492cd",
+//     classCode: "CS146",
+//     section: "02",
+//     className: "Data Structures and Algorithms",
+//     meetingDay: "Tuesday",
+//     meetingStart: "3:00",
+//     meetingEnd: "4:15",
+//   },
+//   {
+//     classid: 3333,
+//     color: "#efb14e",
+//     classCode: "CMPE188",
+//     section: "01",
+//     className: "Machine Learning and Big Data",
+//     meetingDay: "Tuesday",
+//     meetingStart: "10:00",
+//     meetingEnd: "11:15",
+//   },2024-04-12 09:06:39
+// ];
+
+const prisma = new PrismaClient();
+
+export default function Courses({ data }) {
   return (
     <>
       <div className={styles["courses-container"]}>
         <Navbar />
         <div>
-          {" "}
           <div className={styles["top-container"]}>
-            <span className={styles["top-container__title"]}>Your Courses</span>
+            <span className={styles["top-container__title"]}>
+              {" "}
+              Your Courses
+            </span>
             <DividerSvg />
             <button className={styles["add-class-button"]}>
               Add a class <FaPlus color={"#202020"} size={17} />{" "}
             </button>
           </div>
           <div className={styles["courses-container__content"]}>
-            <Course
-              color="#3492cd"
-              classCode="CS146"
-              section="02"
-              className="Data Structures and Algorithms"
-              meetingDay="Tues"
-              meetingTime="3:00-4:00"
-            />
-            <Course
-              color="#f4b030"
-              classCode="CS146"
-              section="02"
-              className="Data Structures and Algorithms"
-              meetingDay="Tues"
-              meetingTime="3:00-4:00"
-            />
-            <Course
-              color="#f4b030"
-              classCode="CS146"
-              section="02"
-              className="Data Structures and Algorithms"
-              meetingDay="Tues"
-              meetingTime="3:00-4:00"
-            />
-            <Course
-              color="#f4b030"
-              classCode="CS146"
-              section="02"
-              className="Data Structures and Algorithms"
-              meetingDay="Tues"
-              meetingTime="3:00-4:00"
-            />
-            <Course
-              color="#f4b030"
-              classCode="CS146"
-              section="02"
-              className="Data Structures and Algorithms"
-              meetingDay="Tues"
-              meetingTime="3:00-4:00"
-            />
+            <CourseCards courseData={data} />
           </div>
         </div>
       </div>
@@ -66,6 +56,37 @@ export default function Courses() {
   );
 }
 
+function CourseCards({ courseData }) {
+  return courseData.map((data) => {
+    const title = `${data.classCode}-${data.section} ${data.className}`;
+    const meetingTime = `${data.meetingStart}-${data.meetingEnd}`;
+    return (
+      <Link
+        href={{
+          pathname: "/course/[id]",
+          query: {
+            id: parseInt(data.classid),
+            title: title,
+            meetingStart: data.meetingStart,
+            meetingEnd: data.meetingEnd,
+            meetingDay: data.meetingDay,
+          },
+        }}
+      >
+        <Course
+          color={data.color}
+          classCode={data.classCode}
+          section={data.section}
+          className={data.className}
+          meetingDay={data.meetingDay}
+          meetingTime={meetingTime}
+        />
+      </Link>
+    );
+  });
+}
+
+// concerned with purely displaying data
 function Course({
   color,
   classCode,
@@ -94,4 +115,44 @@ function Course({
       </span>
     </div>
   );
+}
+
+function parseClassData(classes) {
+  return classes.map((data) => ({
+    classid: data.classid,
+    section: "02",
+    classCode: "CMPE188",
+    className: data.class_name,
+    meetingDay: convertToWeekday(data.days),
+    meetingStart: "3:00",
+    meetingEnd: "4:00",
+    color: `#${data.color}`,
+  }));
+}
+
+function convertToWeekday(num) {
+  const weekday = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  return weekday[num];
+}
+
+export async function getServerSideProps() {
+  // Grab all students for this class
+  const classesResponse = await prisma.classes.findMany({});
+
+  // console.log(classesResponse);
+  const parsedData = parseClassData(classesResponse);
+  console.log(parsedData);
+  return {
+    props: {
+      data: parsedData,
+    },
+  };
 }
